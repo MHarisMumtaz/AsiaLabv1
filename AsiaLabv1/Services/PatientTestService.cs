e690d386a4cc505b643bd83a7a1e093733e39d54
Ahmed Zaki <syedz74@gmail.com>
2016-05-25 00:03:44 +0500

    Technician

--- AsiaLabv1/Services/PatientTestService.cs
+++ AsiaLabv1/Services/PatientTestService.cs
@@ -9,10 +9,30 @@ namespace AsiaLabv1.Services
     public class PatientTestService
     {
         Repository<PatientTest> _PatientTestService = new Repository<PatientTest>();
+        Repository<TestSubcategory> _TestSubCategoryRepository = new Repository<TestSubcategory>();
+        Repository<Patient> _PatientRepository = new Repository<Patient>();
+        Repository<PatientTestResult> _PatientTestResultRepository= new Repository<PatientTestResult>();
 
         public void Add(PatientTest Patienttest)
         {
             _PatientTestService.Insert(Patienttest);
+        }
+        public List<PatientTest> GetPatientTests()
+        {
+            var query = (from pt in _PatientTestService.Table
+                         join p in _PatientRepository.Table
+                         on pt.PatientId equals p.Id
+                         join t in _TestSubCategoryRepository.Table
+                         on pt.TestSubcategoryId equals t.Id
+                         where !_PatientTestResultRepository.Table.Any(ptr => ptr.PatientTestId == pt.Id)
+                         select pt).ToList<PatientTest>();
+             
+            return query;
+        }
+
+        public void SubmitResults(PatientTestResult model) 
+        {
+            _PatientTestResultRepository.Insert(model);
         }
     }
 }
